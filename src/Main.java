62274e83113debf119477d3796eaa2989ad6d363
LuytBui <luytbq43@gmail.com>
2022-01-05 10:54:43 +0700

    done

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Các số nguyên tố bé hơn 100 là: " );
+        for (int i = 2; i< 100; i++){
+            if (isPrime(i)){
+                System.out.printf("%d ", i);
+            }
+        }
+    }
+
+    public static boolean isPrime(int number)
+        if (number < 2) {
+            return false;
+        }
+        if (number == 2) {
+            return true;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
